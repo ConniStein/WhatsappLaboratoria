8932eaa6d6e794d3d223fc288c7f7d61d3483683
ConniStein <cvillagranpoblete1994@gmail.com>
2016-07-21 14:04:25 -0400

    primer commit

---
+++ js/data.js
@@ -0,0 +1,146 @@
+var data = [
+  {
+    "nombre" : "Laboratoria Perú",
+    "hora" : "14:25",
+    "avatar" : "image/logocodeacademy.png",
+    "mensaje": "Dale dale!!",
+    "mensajes" : [
+      {
+        "texto" : "Cuando vamos al workshop?",
+        "hora" : "11:12"
+      },
+      {
+        "texto" : "Les parece el sábado?",
+        "hora" : "11:13"
+      },
+      {
+        "texto" : "Es la única opción >.<",
+        "hora" : "11:16"
+      },
+      {
+        "texto" : "Dale dale!!",
+        "hora" : "14:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "Raymi Saldomando",
+    "hora" : "11:25",
+    "avatar" : "image/raymi.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  }, 
+   {
+    "nombre" : "ana maria",
+    "hora" : "11:25",
+    "avatar" : "image/anamaria.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "aldo",
+    "hora" : "11:25",
+    "avatar" : "image/aldo.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "andrea",
+    "hora" : "11:25",
+    "avatar" : "image/andrea.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "katy",
+    "hora" : "11:25",
+    "avatar" : "image/katy.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "mariana",
+    "hora" : "11:25",
+    "avatar" : "image/mariana.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "rudolf",
+    "hora" : "11:25",
+    "avatar" : "image/rodulfo.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "Jose",
+    "hora" : "11:25",
+    "avatar" : "image/jose.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "Gian",
+    "hora" : "11:25",
+    "avatar" : "image/gian.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "Raymi Saldomando",
+    "hora" : "11:25",
+    "avatar" : "image/raymi.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  }
+];
